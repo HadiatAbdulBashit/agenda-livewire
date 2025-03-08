041003c1c4b6bbba99ce9537ce9ef180bcd6f616
Hadiat Abdul Bashit <hadiatab16@gmail.com>
2025-03-08 09:45:29 +0700

    feat: add edit mode, update and delete on plan list

--- app/Livewire/ListPlanComponent.php
+++ app/Livewire/ListPlanComponent.php
@@ -9,8 +9,34 @@ use Livewire\Component;
 class ListPlanComponent extends Component
 {
     public $plans;
-    public $editMode = false;
-    public $selectedPlanId;
+    public $selectedPlanId = '';
+    public $updatedTask = '';
+
+    public function updatePlan()
+    {
+        // dd($this->selectedPlanId);
+        $plan = Plan::find($this->selectedPlanId);
+        $plan->task = $this->updatedTask;
+        $plan->save();
+        $this->selectedPlanId = '';
+        $this->updatedTask = '';
+    }
+
+    public function completePlan($id)
+    {
+        // dd($id);
+        $plan = Plan::find($id);
+        $plan->is_completed = !$plan->is_completed;
+        $plan->save();
+        $this->dispatch('loadPlans');
+    }
+
+    public function editMode($id)
+    {
+        // dd($id);
+        $this->selectedPlanId = $id;
+        $this->updatedTask = Plan::find($id)->task;
+    }
 
     public function deletePlan($id)
     {
@@ -22,7 +48,7 @@ class ListPlanComponent extends Component
     #[On('loadPlans')]
     public function render()
     {
-        $this->plans = Plan::all()->sortBy('priority');
+        $this->plans = Plan::all()->sortBy('priority')->where('is_completed', false);
         return view('livewire.list-plan-component');
     }
 }
